1c70c58047aa25c752cfcfee866b96e55c2ca20a
xDrakeFP <fpini3q@gmail.com>
2025-10-24 10:09:06 +0200

    Initial commit

---
+++ src/main/java/federicopini/B7_L5/B7L5Application.java
@@ -0,0 +1,13 @@
+package federicopini.B7_L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/federicopini/B7_L5/B7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package federicopini.B7_L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
